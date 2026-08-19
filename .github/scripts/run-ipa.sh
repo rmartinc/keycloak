e49d6f8b40a1a4492c5aa0d023fdcc59fdf88757
rmartinc <rmartinc@redhat.com>
2026-08-19 09:39:42 +0200

    test

--- .github/scripts/run-ipa.sh
+++ .github/scripts/run-ipa.sh
@@ -11,8 +11,8 @@ EOF'
 sudo systemctl restart docker
 
 echo "Starting ipa-server container"
-sudo chown -R runner:100000 "$HOME/.m2"
-sudo chown -R runner:100000 "$1"
+sudo chown -R 100000 "$HOME/.m2"
+sudo chown -R 100000 "$1"
 container=$($DOCKER run --detach -h ipa.example.test --sysctl net.ipv6.conf.all.disable_ipv6=0 --workdir /github/workspace -v "$1":"/github/workspace" -v "$HOME/.m2":"/root/.m2" freeipa/freeipa-server:rocky-9 ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000)
 
 echo "Container $container started, waiting ipa-server configuration"
