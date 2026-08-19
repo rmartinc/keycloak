eab3a5eda5f280c5e6d7b991caf60ae27afbcde4
rmartinc <rmartinc@redhat.com>
2026-08-19 08:45:40 +0200

    test

--- .github/scripts/run-ipa.sh
+++ .github/scripts/run-ipa.sh
@@ -9,15 +9,8 @@ sudo bash -c 'cat << EOF > /etc/docker/daemon.json
 EOF'
 sudo systemctl restart docker
 
-if [ -f "$HOME/ipa-data.tar" ]; then
-  echo "Using data from previous execution"
-  sudo tar xpf "$HOME/ipa-data.tar" -C "$HOME"
-else
-  mkdir "$HOME/ipa-data"
-fi
-
 echo "Starting ipa-server container"
-container=$($DOCKER run --detach --rm --userns=host -h ipa.example.test --sysctl net.ipv6.conf.all.disable_ipv6=0 --workdir /github/workspace -v "$HOME/ipa-data":"/data":Z -v "$1":"/github/workspace" -v "$HOME/.m2":"/root/.m2" freeipa/freeipa-server:rocky-9 ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000)
+container=$($DOCKER run --detach --rm --userns=host -h ipa.example.test --sysctl net.ipv6.conf.all.disable_ipv6=0 --workdir /github/workspace -v "$1":"/github/workspace" -v "$HOME/.m2":"/root/.m2" freeipa/freeipa-server:rocky-9 ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000)
 
 echo "Container $container started, waiting ipa-server configuration"
 sleep 30
@@ -40,11 +33,6 @@ $DOCKER exec $container .github/scripts/run-ipa-tests.sh $new_install
 result=$?
 
 $DOCKER stop $container
-
-if [ $result -eq 0 ]; then
-  echo "Doing a backup of the ipa-data directory for caching"
-  sudo tar cpf "$HOME/ipa-data.tar" -C "$HOME" ipa-data
-fi
 
 echo "Tests executed with result: $result"
 exit $result
